aba2c691b7ab8d4fda4c896c96086a2d56898468
katemejaski <katmackjj@gmail.com>
2021-05-17 15:54:52 -0400

    Initial commit

---
+++ src/main/java/com/tts/ValidationLab/ValidationLabApplication.java
@@ -0,0 +1,13 @@
+package com.tts.ValidationLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationLabApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ValidationLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/ValidationLab/ValidationLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.ValidationLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
